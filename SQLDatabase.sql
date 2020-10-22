3e3ec1c64369a932bb2d0597dadf46fa825034d4
roched1atwit <43098349+roched1atwit@users.noreply.github.com>
2020-10-22 16:13:04 -0400

    Update SQLDatabase.sql
    Finished Basic DB Setup

--- SQLDatabase.sql
+++ SQLDatabase.sql
@@ -2,34 +2,47 @@ CREATE DATABASE BostonCode;
 
 
 create table Speaker (
-speakerID int,
-lastName varchar(255),
-dayContact varchar(255),
-phoneNumber varchar(12),
-email varchar(255)
+speaker_id int,
+name varchar(255),
+day_of_contact varchar(255),
+phone_number varchar(12),
+email varchar(255),
+  
+PRIMARY KEY (speaker_id)
 
 );
 
 create table Room (
-roomID int,
-roomNumber varchar(255),
+room_id int,
+room_number varchar(255),
 seats int,
 
-
+PRIMARY KEY (room_id)
+  
 );
 
-/*i didnt create the session table just did have of it
+create table Time_Slot(
+time_slot_id int,
+time_start time,
+time_end time,
+  
+PRIMARY KEY (time_slot_id)
+  
+);
+
 create table Session (
-sessionID int,
-PRIMARY KEY (sessionID),
-sessionName varchar(255),
-timeSlot varchar(255),
-roomID int,
+session_id int,
+session_name varchar(255),
+time_slot_id int,
+room_id int,
+speaker_id int,
 
-FOREIGN KEY (roomID) REFERENCES Room(roomID)
+PRIMARY KEY (session_id),
+FOREIGN KEY (room_id) REFERENCES Room(room_id),
+FOREIGN KEY (time_slot_id) REFERENCES Time_Slot(time_slot_id),
+FOREIGN KEY (speaker_id) REFERENCES Speaker(speaker_id)
 
 );
-*/
 
 
 
