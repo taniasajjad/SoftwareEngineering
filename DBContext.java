aea7a8a30d55e5fdd3fe3f3af6278a43e09e074c
taniasajjad <48694852+taniasajjad@users.noreply.github.com>
2020-10-31 11:31:33 -0400

    Add files via upload

---
+++ DBContext.java
@@ -0,0 +1,39 @@
+package testdbsering;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+ 
+public class DBContext {
+
+    private final String severName = "localhost";
+    private final String dbName = "sqldatabase";
+    private final String portNumber = "3306";
+    private final String userID = "admin";
+    private final String password = "2020";
+
+    public DBContext() {
+    }
+
+    public Connection getconnection() throws Exception {
+        String url = "jdbc:mysql://" + severName + ":" + portNumber + "/" + dbName;
+        return DriverManager.getConnection(url, userID, password);
+    }
+
+    public ResultSet getConnectionStatement(String query) throws Exception {
+        Connection connection = getconnection();
+        Statement statement = connection.createStatement();
+        ResultSet rs = statement.executeQuery(query);
+        return rs;
+    }
+
+    public void preStatement(String query) throws SQLException, Exception {
+        Connection connection = getconnection();
+        PreparedStatement preStatement = connection.prepareStatement(query);
+        preStatement.executeUpdate();
+        connection.close();
+    }
+}
