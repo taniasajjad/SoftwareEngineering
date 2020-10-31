297861cd769dae03ba6cda2fbdac9938782af995
taniasajjad <48694852+taniasajjad@users.noreply.github.com>
2020-10-31 11:32:20 -0400

    Update TestDBSering.java

--- TestDBSering.java
+++ TestDBSering.java
@@ -1,24 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package testdbsering;
 
 import java.sql.Connection;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.sql.ResultSet;
 
-/**
- *
- * @author HaoPhan
- */
 public class TestDBSering {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
         DBContext database = new DBContext();
